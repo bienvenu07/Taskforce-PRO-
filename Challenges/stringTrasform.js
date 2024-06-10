f89196a2484e10b7c08f88ec3a164dec09757684
bienvenu07 <kipsayclopia@gmail.com>
2024-06-10 09:24:44 +0200

    ajout de respensive hover reactif

--- Challenges/stringTrasform.js
+++ Challenges/stringTrasform.js
@@ -1,18 +1,27 @@
 function stringTransform(str) {
     const length = str.length;
-
+  
     if (length % 3 === 0) {
-        return str.split('').reverse().join('');
+      // Reverse the string if length is divisible by 3
+      return str.split('').reverse().join('');
     } else if (length % 5 === 0) {
-        return str.split('').map((char) => char.charCodeAt(0)).join('');
+      // Uppercase every other character if length is divisible by 5
+      return str.split('').map((char, index) => (index % 2 === 0 ? char.toUpperCase() : char)).join('');
     } else if (length % 15 === 0) {
-        return str.split('').reverse().join('').split('').map((char) => char.charCodeAt(0)).join('');
+      // Combine reverse and uppercase logic for length divisible by 15
+      return str.split('')
+        .reverse()
+        .join('')
+        .split('')
+        .map((char, index) => (index % 2 === 0 ? char.toUpperCase() : char))
+        .join('');
     } else {
-        return str;
+      // Return the original string for other lengths
+      return str;
     }
-}
-
-// Exemple d'utilisation
-const inputStr = "Hamburger";
-const transformedStr = stringTransform(inputStr);
-console.log(transformedStr); // regrubmaH
+  }
+  
+  // Example Usage
+  const inputStr = "Hamburger";
+  const transformedStr = stringTransform(inputStr);
+  console.log(transformedStr); // regrubmaH (Correct output)
